380ddf3a4dd8d26c334003d2cc019cf452f2f8d3
chrisvega <cnvegaro@unitru.edu.pe>
2025-04-02 00:29:48 -0500

    Setting button type as no required

--- eslint.config.js
+++ eslint.config.js
@@ -9,45 +9,46 @@ import reactX from 'eslint-plugin-react-x';
 import reactDom from 'eslint-plugin-react-dom';
 
 export default [
-  {
-    ignores: ['dist'],
-  },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  {
-    languageOptions: {
-      ecmaVersion: 2022,
-      globals: globals.browser,
-      parser: tseslint.parser,
-      parserOptions: {
-        sourceType: 'module',
-        project: ['./tsconfig.node.json', './tsconfig.app.json'],
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-    plugins: {
-      react,
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-      'react-x': reactX,
-      'react-dom': reactDom,
-    },
-    settings: {
-      react: {
-        version: 'detect',
-      },
-    },
-    rules: {
-      ...react.configs.recommended.rules,
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-      'react/react-in-jsx-scope': 'off',
-      ...reactX.configs['recommended-typescript'].rules,
-      ...reactDom.configs.recommended.rules,
-    },
-  },
-  prettier,
+	{
+		ignores: ['dist'],
+	},
+	eslint.configs.recommended,
+	...tseslint.configs.recommendedTypeChecked,
+	{
+		languageOptions: {
+			ecmaVersion: 2022,
+			globals: globals.browser,
+			parser: tseslint.parser,
+			parserOptions: {
+				sourceType: 'module',
+				project: ['./tsconfig.node.json', './tsconfig.app.json'],
+				tsconfigRootDir: import.meta.dirname,
+			},
+		},
+		plugins: {
+			react,
+			'react-hooks': reactHooks,
+			'react-refresh': reactRefresh,
+			'react-x': reactX,
+			'react-dom': reactDom,
+		},
+		settings: {
+			react: {
+				version: 'detect',
+			},
+		},
+		rules: {
+			...react.configs.recommended.rules,
+			...reactHooks.configs.recommended.rules,
+			'react-refresh/only-export-components': [
+				'warn',
+				{ allowConstantExport: true },
+			],
+			'react/react-in-jsx-scope': 'off',
+			...reactX.configs['recommended-typescript'].rules,
+			...reactDom.configs.recommended.rules,
+			'react-dom/no-missing-button-type': 'off',
+		},
+	},
+	prettier,
 ];
